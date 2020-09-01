89e3950052619f5a8e0703841ec78d0b68a8356d
Andjela Niketic <andjelaniketic109@gmail.com>
2020-09-01 23:21:10 +0200

    odredjuje polozaj tacke u odnosu na pravu

---
+++ cas12/polozaj_tacke.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+struct tacka
+{
+	int x, y;
+};
+
+void polozaj(tacka A, tacka B, tacka P){
+	int s = (B.y - A.y)*P.x + (A.x - B.x)*P.y + (B.x*A.y - A.x*B.y);
+	if(s > 0)
+		cout << "Tacka je ispod prave\n";
+	else if(s < 0)
+		cout << "Tacka je iznad prave\n";
+	else
+		cout << "Tacka je na pravoj\n";
+}
+int main(){
+	tacka A, B, C;
+
+	cin >> A.x >> A.y >> B.x >> B.y >> C.x >> C.y;
+	polozaj(A, B, C);
+
+	return 0;
+}
